8f6014c127e5a7dcaa18d92c1a9af494975e7133
Ighor Moura <ighor.moura787@gmail.com>
2018-01-24 21:56:21 -0300

    ViaCep Project working well, simply view

---
+++ src/pages/home/home.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/observable';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  public resultApi:any = {};
+  public feedApi:any = {};
+
+  constructor(public navCtrl: NavController,
+              public httpClient: HttpClient) {}
+              
+  callApi():void {
+    this.httpClient.get(`http://viacep.com.br/ws/${this.feedApi.cep}/json/`)
+    .subscribe(
+      data =>{
+        this.resultApi = data;
+        console.log(data);
+      }
+    );
+  }
+}
